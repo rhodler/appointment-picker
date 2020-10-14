7c7f18ed6b47ea7db1eeef890070e52f6b6b055e
Romelus Jean-Marie <romelusjeanmarie.pro@gmail.com>
2020-10-14 07:33:59 +0200

    Updates

---
+++ precache-manifest.b75e1167b8067b24e7b4b1e93271a468.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf21b4b12208d7dcf845f1f491572669",
+    "url": "/appointment-picker/index.html"
+  },
+  {
+    "revision": "5901b6aa7bcf519e354d",
+    "url": "/appointment-picker/static/css/main.de8b1a1b.chunk.css"
+  },
+  {
+    "revision": "75ce425040b8ee110634",
+    "url": "/appointment-picker/static/js/2.13f25fd9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/appointment-picker/static/js/2.13f25fd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5901b6aa7bcf519e354d",
+    "url": "/appointment-picker/static/js/main.4d4c71e8.chunk.js"
+  },
+  {
+    "revision": "5d084c05380ff9d04dc9",
+    "url": "/appointment-picker/static/js/runtime-main.a2c68a12.js"
+  }
+]);
